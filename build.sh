c50ca3e204fadf2ff50717260e9eeab00f17ec03
44670 <44670@users.noreply.github.com>
2022-02-06 17:09:07 +0800

    1.2

--- build.sh
+++ build.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-statik -f -src=./static
-
 rm ProjectSend-*
 export GOARCH=386 GOOS=windows
 go build -o ProjectSend-$GOOS-$GOARCH.exe
@@ -14,7 +12,7 @@ go build -o ProjectSend-$GOOS-$GOARCH
 export GOARCH=amd64 GOOS=linux 
 go build -o ProjectSend-$GOOS-$GOARCH
 
-export GOARCH=arm GOOS=linux GOARM=7
+export GOARCH=arm GOOS=linux GOARM=6
 go build -o ProjectSend-$GOOS-$GOARCH$GOARM
 
 export GOARCH=mipsle GOOS=linux 
